d90c1fda61528656725cf11d006af38333089995
Alexander Truyaert <alexander.truyaert@outlook.com>
2024-12-08 15:09:47 +0100

    read directory that was given as param

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,15 @@
-fn main() {
+fn main() -> () {
+    let location = std::env::args().nth(1).unwrap_or(String::from("."));
+
+    match std::fs::read_dir(&location) {
+        Ok(entries) => {
+            for entry in entries {
+                match entry {
+                    Ok(location) => println!("{:?}", location.path()),
+                    Err(e) => eprintln!("Error: {}", e),
+                }
+            }
+        },
+        Err(e) => eprintln!("Error: {}", e),
+    }
 }
