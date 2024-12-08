35370e15b93d57a1b1987ee7bf96407d7317dbeb
Alexander Truyaert <alexander.truyaert@outlook.com>
2024-12-08 16:38:42 +0100

    skip hidden files for now

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,26 @@
-fn main() -> () {
-    let location = std::env::args().nth(1).unwrap_or(String::from("."));
+use anyhow;
 
-    match std::fs::read_dir(&location) {
-        Ok(entries) => {
-            for entry in entries {
-                match entry {
-                    Ok(location) => println!("{:?}", location.path()),
-                    Err(e) => eprintln!("Error: {}", e),
-                }
+fn main() -> anyhow::Result<()> {
+    let location = std::env::args().nth(1).unwrap_or(String::from("."));
+    let dir = std::fs::read_dir(&location)?;
+
+    let entries_to_show = dir
+        .filter_map(|entry| {
+            let entry = entry.ok()?;
+
+            if entry.file_name().into_string().ok()?.starts_with(".") {
+                return None;
             }
-        },
-        Err(e) => eprintln!("Error: {}", e),
-    }
+
+            return Some(entry);
+        })
+        .collect::<Vec<_>>();
+
+    entries_to_show
+        .iter()
+        .for_each(|entry| print!("{:?}", entry.file_name()));
+
+    println!("");
+
+    return Ok(());
 }
