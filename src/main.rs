1f36f8145fa57e75c8dd98c69b65aafe4cbd44cd
Alexander Truyaert <alexander.truyaert@outlook.com>
2024-12-09 19:39:48 +0100

    added colors to different file types

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,32 @@
+use std::fmt::Display;
+
 use anyhow;
+use colored::*;
+
+struct File {
+    pub entry: std::fs::DirEntry
+}
+
+impl Display for File {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let file_path = match self.entry.file_name().into_string() {
+            Ok(path) => path,
+            Err(_) => return write!(f, "Unable to read the file"),
+        };
+
+        let metadata = match self.entry.metadata() {
+            Ok(metadata) => metadata,
+            Err(_) => return write!(f, "Unable to extract the metadata"),
+        };
+
+        match metadata {
+            _ if metadata.is_file() => return write!(f, "{}", file_path.white()),
+            _ if metadata.is_dir() => return write!(f, "{}", file_path.blue()),
+            _ if metadata.is_symlink() => return write!(f, "{}", file_path.cyan()),
+            _ => return write!(f, "{}", file_path.white()),
+        };
+    }
+}
 
 fn main() -> anyhow::Result<()> {
     let location = std::env::args().nth(1).unwrap_or(String::from("."));
@@ -12,18 +40,17 @@ fn main() -> anyhow::Result<()> {
                 return None;
             }
 
-            return Some(entry);
+            return Some(File { entry });
         })
         .collect::<Vec<_>>();
 
-    entries_to_show.sort_by(|a, b| a.path().cmp(&b.path()));
+    entries_to_show.sort_by(|a, b| a.entry.path().cmp(&b.entry.path()));
 
     let entries_to_show = entries_to_show;
 
     entries_to_show
         .iter()
-        .filter_map(|entry| entry.file_name().into_string().ok())
-        .for_each(|name| print!("{} ", name));
+        .for_each(|file_entry| print!("{} ", file_entry));
 
 
     println!("");
